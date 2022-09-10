493a4991d8c87a42209b0e61d1d12f823b2fdb1c
Hope <plutohope8@gmail.com>
2022-09-09 17:38:45 -0700

    22

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,8 +8,8 @@
  * Description: prints the last digit of random number
  *
  * Return: Always 0 (Success)
- * 
- */ 
+ *
+ */
 int main(void)
 {
 	int n;
@@ -25,6 +25,5 @@ int main(void)
 		printf("Last digit of %i is %i and is 0\n", n, last_num);
 	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_num);
-	
-	return 0;
+	return (0);
 }
