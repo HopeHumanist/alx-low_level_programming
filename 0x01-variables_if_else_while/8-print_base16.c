960e2b9c173a5c9f06f96bffbc6ac8e951c499af
Hope <plutohope8@gmail.com>
2022-09-12 15:30:25 -0700

    809

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - ikizo
+ * Description: Prints all the # in base 16
+ *
+ * Return: 0 (Eurika)
+ */
+
+int main(void)
+{
+	char hex;
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	for (hex = 'a'; hex <= 'g'; hex++)
+		putchar(hex);
+	putchar('\n');
+	return (0);
+}
