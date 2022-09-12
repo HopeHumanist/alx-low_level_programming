40c65b7b2775153b47c1ef4d8db3db6c46410df6
Hope <plutohope8@gmail.com>
2022-09-12 15:02:30 -0700

    32

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - ikizo
+ *
+ * Description: Prints all single digits
+ *
+ * Return: 0 (Eurika)
+ */
+
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
