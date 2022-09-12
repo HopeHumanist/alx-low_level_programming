ed19aa32887ba73b147fc1f14eb1168604feeb05
Hope <plutohope8@gmail.com>
2022-09-12 13:07:43 -0700

    65

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,8 @@ int main (void)
 {
 	printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
 	printf("Size of a char: %i byte(s)\n", (int) sizeof(int));
+	printf("Size of a char: %i byte(s)\n", (int) sizeof(long int));
+	printf("Size of a char: %i byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a char: %i byte(s)\n", (int) sizeof(float));
-	printf("Size of a char: %i byte(s)\n", (int) sizeof(double));
 	return (0);
 }
