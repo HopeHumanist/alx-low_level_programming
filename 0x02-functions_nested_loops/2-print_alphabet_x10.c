ab6bcd1ebccaf60b9d484931df321870aafc64b5
Hope <plutohope8@gmail.com>
2022-09-13 14:42:38 -0700

    31

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Hajime
+ *
+ * Description: prints alpha x10
+ *
+ * Return: 0 (Baga)
+ */
+
+void print_alphabet_x10(void)
+{
+	char K, J;
+
+	for (K = 1; K <= 10; K++)
+		{
+		for (J = 'a'; J <= 'z'; J++)
+			_putchar(J);
+		}
+	_putchar('\n');
+}
