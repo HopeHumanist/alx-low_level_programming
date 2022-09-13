9b8ec484eebd53a13cab11c617f20a7811833b7b
Hope <plutohope8@gmail.com>
2022-09-13 15:16:57 -0700

    HJ

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** int_islower(int c) - Ikizo
+ *
+ * Description: checks for lower case char
+ *
+ * Return: 1 if lowercase or 0 if anything else
+ */
+
+int_islower(int c)
+{
+	if (c >= '97'; c <= '122')
+		return (1);
+	return (0);
+}
