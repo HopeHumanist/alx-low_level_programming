0a7fa476bb7395b05b97712af1c8bbf5de47f16a
Hope <plutohope8@gmail.com>
2022-09-13 14:16:26 -0700

    54

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Hajime
+ *
+ * Description: prints the alpha in lowercase
+ *
+ * Return: 0 (Baga)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++);
+		_putchar(low);
+	
+	return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define _PUTCHAR 
 
 int _putchar(char c);
-
+void print_alphabet(void);
 
 #endif
