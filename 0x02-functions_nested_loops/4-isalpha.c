7191ecd5039340bc0a9393b97c591e49fcebe6b0
Hope <plutohope8@gmail.com>
2022-09-13 16:04:08 -0700

    600

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - lets begin
+ * @c: char to be checked
+ *
+ * Description: checks if alpha
+ *
+ * Return: 1 if alpha or 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122); (c >= 65 && c <= 90);
+		return (1);
+	return (0);
+}
