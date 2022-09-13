5e2cd3cd5f923cf1d9e45f3c1432ef52b93311a6
Hope <plutohope8@gmail.com>
2022-09-13 14:54:02 -0700

    345

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,8 +14,8 @@ void print_alphabet_x10(void)
 
 	for (K = 1; K <= 10; K++)
 		{
-		for (J = 'a'; J <= 'z'; J++)
-			_putchar(J);
-			_putchar('\n');
+			for (J = 'a'; J <= 'z'; J++)
+				_putchar(J);
+				_putchar('\n');
 		}
 }
