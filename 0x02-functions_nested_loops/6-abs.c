5291d8bf37f28151833b8839ba12cd4a651a79d6
Hope <plutohope8@gmail.com>
2022-09-14 14:49:36 -0700

    564

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - ikizo
+ *
+ * Description: calculates the abs
+ *  @h: number to be calculated
+ *
+ *  Return: 0 (Yondaime)
+ */
+
+int _abs(int h)
+{
+	if (h > 0)
+		return (0);
+	else
+		return (abs(h));
+}
