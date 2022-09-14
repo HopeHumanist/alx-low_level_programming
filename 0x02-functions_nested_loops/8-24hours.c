573a4128cb9072378ff3910187121fc0532db5ff
Hope <plutohope8@gmail.com>
2022-09-14 16:45:28 -0700

    786

---
+++ 0x02-functions_nested_loops/8-24hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of day of jack bauer
+ *
+ * Description: prints a list of time
+ *
+ * Return: 0 (Bazinga)
+ */
+void jack_bauer(void)
+{
+	int h, k;
+
+	for (h = 0; k < 24; k++)
+	{
+		for (k = 0; k < 60; k++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+			_putchar(':');
+		}
+	}
+}
