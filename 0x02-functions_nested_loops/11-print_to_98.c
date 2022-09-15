d12c371646282368f23663f6b434c3eca1c74a12
Hope <plutohope8@gmail.com>
2022-09-15 06:23:25 -0700

    45

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,34 +1,39 @@
 #include "main.h"
 #include <stdio.h>
- /**
-  * print_to_98 - Print all natural numbers from n to 98
-  * @n: int type number
-  */
+
+/**
+ * print_to_98 - prints all natural numbers
+ * @n: int type number
+ * Description: prints all natural numbers
+ *
+ * Return: 0 (Bazinga)
+ */
+
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n > 98)
 	{
-		while (n >= 98)
-		 {
-		         printf("%d", n);
-		         if (n != 98)
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
 			{
-			        printf(", ");
+				printf(", ");
 			}
 			n--;
-			}
+		}
 	}
 	else
 	{
-		while (n <= 98)
+		while (n <= 98)
 		{
-			printf("%d", n);
-			if (n != 98)
+			printf("%d", n);
+			if (n != 98)
 			{
-			printf(", ");
+				printf(", ");
 			}
 			n++;
 		}
-	}
 	printf("\n");
+	}
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 {
 	int h, k;
 
-	for (h = 0; k < 24; k++)
+	for (h = 0; h < 24; k++)
 	{
 		for (k = 0; k < 60; k++)
 			_putchar(h / 10 + '0');
@@ -20,6 +20,5 @@ void jack_bauer(void)
 			_putchar(k % 10 + '0');
 			_putchar(':');
 			_putchar('\n');
-		
 	}
 }
