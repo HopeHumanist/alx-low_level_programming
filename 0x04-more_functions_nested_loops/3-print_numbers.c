00dade4ca37e2c7a5ffe06fdf9b40d409b5461c7
Hope <plutohope8@gmail.com>
2022-09-15 16:14:56 -0700

    45

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints #s
+ *
+ * Description: prints #s 0-9
+ *
+ * Return: 0 (Bazinga)
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+		for (c = 0; c <= 9; c++)
+	{
+			_putchar('c');
+			_putchar('\n');
+	}
+}
