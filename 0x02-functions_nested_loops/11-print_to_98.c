69141e728c40f67ddd3b92e104d3a6d1c6686b58
Hope <plutohope8@gmail.com>
2022-09-14 17:47:23 -0700

    430

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,34 @@
-#include "main.h"
-
-/**
- * print_to_98 - lets begin
- * @n: number
- *
- * description: prints natural numbers
- *
- * Return: 0 (Bazinga)
- */
-
-void print_to_98(int n)
-{
-	for (n = 0; n <= 98; n++)
-		_putchar(n);
-}
-
+#include "main.h" 
+ #include <stdio.h> 
+ /** 
+  * print_to_98 - Print all natural numbers from n to 98 
+  * @n: int type number 
+  */ 
+ void print_to_98(int n) 
+ { 
+         if (n > 98) 
+         { 
+                 while (n >= 98) 
+                 { 
+                         printf("%d", n); 
+                         if (n != 98) 
+                         { 
+                                 printf(", "); 
+                         } 
+                         n--; 
+                 } 
+         } 
+         else 
+         { 
+                 while (n <= 98) 
+                 { 
+                         printf("%d", n); 
+                         if (n != 98) 
+                         { 
+                                 printf(", "); 
+                         } 
+                         n++; 
+                 } 
+         } 
+         printf("\n"); 
+ }
