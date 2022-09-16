e7c4970646605e7c1f3ba09008c69b4b6e4da2f5
Hope <plutohope8@gmail.com>
2022-09-16 10:58:24 -0700

    89

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - Hajime
+ *
+ * Return: 0 (Bazinga)
+ *
+ */
+
+void more_numbers(void)
+{
+	int j, k;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (k = 0; k <= 14; k++)
+			_putchar(k);
+	}
+	_putchar('\n');
+}
