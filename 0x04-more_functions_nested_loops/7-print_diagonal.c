f9be9c995f92656a1e6109e0491d18efe7a2521e
Hope <plutohope8@gmail.com>
2022-09-16 14:20:00 -0700

    89

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Hajime
+ * @n: input
+ *
+ * Return: 0 (Bazinga)
+ */
+
+void print_diagonal(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('\\');
+		_putchar('\n');
+	}
+		else (n <= 0);
+			_putchar('\n');
+}
