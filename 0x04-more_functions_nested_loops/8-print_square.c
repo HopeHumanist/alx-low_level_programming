a0b2762945cc1690ec2e997bd08cf130a7f2cf7f
Hope <plutohope8@gmail.com>
2022-09-17 06:13:20 -0700

    7

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - lets begin
+ * @size: size of the square
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int h, j;
+
+		for (h = 0; h < size; h++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the entry
+ *
+ * Description: prints fizz, buzz & fizzbuzz
+ *
+ * Return: 0 (Bazinga)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	pritnf("\n");
+	return (0);
+}
+
