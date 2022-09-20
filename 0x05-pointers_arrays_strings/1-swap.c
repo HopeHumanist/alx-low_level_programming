3a687b119d62c0dab39a61f531471522a42fda33
Hope <plutohope8@gmail.com>
2022-09-20 14:32:53 -0700

    78

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Hajime
+ * @a: input #1
+ * @b: input #2
+ * Returns: Nada
+ */
+
+void swap_int(int *a, int *b);
+{
+	int hj;
+
+	hj = *a;
+	*a = *b;
+	*b = hj;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Frigging pointers, strings and arrays
