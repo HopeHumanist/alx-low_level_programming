4c786181301dc26e81ee79a4bffe357d38c15a36
Hope <plutohope8@gmail.com>
2022-09-20 14:55:51 -0700

    .78

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Pakwambila
+ * @s: string
+ * Return: C (Eurika)
+ */
+
+int _strlen(char *s)
+{
+	int C
+
+	for (C = 0; s[C] != '\0'; C++);
+
+	return (C);
+}
