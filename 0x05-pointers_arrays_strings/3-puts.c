cc2893ecd082c2793a48af895a35d269a69298de
Hope <plutohope8@gmail.com>
2022-09-20 15:30:40 -0700

    89

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Olo
+ * @str: input
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	int j;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
