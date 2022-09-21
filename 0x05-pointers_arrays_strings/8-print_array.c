6bc4d487a5f15ec6324140c552d0d98d08298ef0
Hope <plutohope8@gmail.com>
2022-09-21 04:10:06 -0700

    .98

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Hajime
+ * @a: pointer
+ * @n: number
+ * Return: nada
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
