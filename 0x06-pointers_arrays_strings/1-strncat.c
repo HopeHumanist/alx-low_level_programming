0574d120077262aaf3d1b415afbe55f740c51dd9
Hope <plutohope8@gmail.com>
2022-09-22 06:44:09 -0700

    91

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - Ikizo
+ * @src: str #
+ * @dest: str ##
+ * @n: #
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = strlen(dest);
+	int a = 0;
+
+	while (a < n && *src)
+	{
+		dest[index + a] = *src;
+		src++;
+		a++;
+	}
+
+	dest[index + a] = '\0';
+	return (dest);
+}
+
